eb53e580beea041cbc397985143c21ac79a65c2d
DESKTOP-I7VRPC5\JongWoo <jinlaove17@naver.com>
2024-10-03 18:54:17 +0900

    [1주차][전종우] add&fix: 리스트 수정 기능 구현, 리스트 보기 방식 및 개수 표기 기능 추가, 리스트 관련 이미지 파일 교체, 모달 애니메이션 추가, 리스트 추가시 날짜, 내용 유효성 검사 추가

--- TodoList/전종우/Vanilla/js/to_do_list.js
+++ TodoList/전종우/Vanilla/js/to_do_list.js
@@ -1,16 +1,90 @@
 let list = [];
 
-function toggleListMaker(open) {
+window.onload = function () {
+  let viewForm = document.getElementById("view-type-form");
+
+  viewForm.addEventListener("change", (event) => {
+    updateList();
+  });
+
   let listMaker = document.getElementById("list-maker");
 
-  listMaker.setAttribute("style", open ? "display: block" : "display: none");
+  // animationstart 이벤트가 발생할 때 이미 요소가 display: none 상태로 숨겨져 있기 때문입니다.
+  // display: none 상태에서는 요소가 DOM 트리에는 존재하지만 렌더링되지 않기 때문에,
+  // 애니메이션이 시작되지 않으며 animationstart 이벤트도 발생하지 않습니다.
+  // listMaker.addEventListener("animationstart", (event) => {
+  //   if (event.animationName === "openModal") {
+  //     listMaker.style.display = "block";
+  //   }
+  // });
+
+  // 모달 애니메이션 끝난 후 display 속성을 none으로 설정
+  listMaker.addEventListener("animationend", (event) => {
+    if (event.animationName === "closeModal") {
+      listMaker.style.display = "none";
+    }
+  });
+};
+
+// 등록과 수정을 동일한 DOM 요소로 해결하기 위해 매개변수에 modifyIndex를 추가함
+// toggleListMaker(true);와 같이 호출 시, 등록
+function toggleListMaker(open, modifyIndex) {
+  let listMaker = document.getElementById("list-maker");
+
+  if (open) {
+    let title = document.querySelector("#list-maker h1");
+    let date = document.getElementById("to-do-date");
+    let content = document.getElementById("to-do-content");
+    let addEntityBtn = document.getElementById("add-entity-button");
+    let modifyEntityBtn = document.getElementById("modify-entity-button");
+
+    if (modifyIndex === undefined) {
+      title.innerText = "할 일을 등록해 주세요.";
+      date.value = "";
+      content.value = "";
+
+      // 등록 버튼 활성화, 수정 버튼 비활성화
+      addEntityBtn.setAttribute("style", "display: inline-block");
+      modifyEntityBtn.setAttribute("style", "display: none");
+    } else {
+      title.innerText = "할 일을 수정해 주세요.";
+      date.value = list[modifyIndex].date;
+      content.value = list[modifyIndex].content;
+
+      // 등록 버튼 비활성화, 수정 버튼 활성화
+      addEntityBtn.setAttribute("style", "display: none");
+      modifyEntityBtn.setAttribute("style", "display: inline-block");
+      modifyEntityBtn.onclick = function () {
+        modifyEntity(modifyIndex);
+      };
+    }
+
+    listMaker.style.display = "block";
+    listMaker.style.animation = "openModal 0.3s forwards";
+  } else {
+    listMaker.style.animation = "closeModal 0.3s forwards";
+  }
 }
 
 function addEntity() {
+  let date = document.getElementById("to-do-date").value;
+
+  if (date.trim() === "") {
+    alert("날짜를 입력하세요");
+    return;
+  }
+
+  let content = document.getElementById("to-do-content").value;
+
+  if (content.trim() === "") {
+    alert("내용을 입력하세요");
+    return;
+  }
+
   let obj = {
     finished: 0,
-    date: document.getElementById("to-do-date").value,
-    content: document.getElementById("to-do-content").value,
+    date: date,
+    content: content,
   };
 
   list.push(obj);
@@ -18,7 +92,28 @@ function addEntity() {
   toggleListMaker(false);
 }
 
+function modifyEntity(modifyIndex) {
+  let date = document.getElementById("to-do-date");
+  let content = document.getElementById("to-do-content");
+  let listContent = document.querySelectorAll(
+    `#list-contents > div:nth-of-type(${modifyIndex + 1}) p`
+  );
+
+  listContent[1].innerText = list[modifyIndex].content = content.value;
+
+  // 날짜를 변경했을 때만 전체 리스트 갱신
+  if (list[modifyIndex].date !== date.value) {
+    listContent[0].innerText = list[modifyIndex].date = date.value;
+    updateList();
+  }
+
+  toggleListMaker(false);
+}
+
 function updateList() {
+  let viewForm = document.getElementById("view-type-form");
+  let viewType = viewForm["view-type"].value;
+
   list.sort((a, b) => {
     if (a.finished === b.finished) {
       return a.date < b.date ? -1 : 1;
@@ -31,7 +126,22 @@ function updateList() {
 
   listContents.innerHTML = ``;
 
-  for (let i = 0; i < list.length; ++i) {
+  loop: for (let i = 0; i < list.length; ++i) {
+    switch (viewType) {
+      case "view-to-do":
+        if (list[i].finished) {
+          continue loop;
+        }
+
+        break;
+      case "view-complete":
+        if (!list[i].finished) {
+          continue loop;
+        }
+
+        break;
+    }
+
     let div = document.createElement("div");
 
     div.classList.add("to_do_list-box");
@@ -41,12 +151,15 @@ function updateList() {
     );
     div.innerHTML = `
     <div class="list-content">
-      <p>${list[i].date}</p>
+      <div class="flex-row-between-container">
+        <p>${list[i].date}</p>
+        <div>
+          <button><img src="img/check.png" alt=""></button>
+          <button><img src="img/edit-tool.png" alt=""></button>
+          <button><img src="img/delete.png" alt=""></button>
+        </div>
+      </div>
       <p>${list[i].content}</p>
-    </div>
-    <div class="flex-row-right-container">
-      <button><img src="img/CheckBoxButton.png" alt=""></button>
-      <button><img src="img/CloseButton.png" alt=""></button>
     </div>
     `;
 
@@ -58,12 +171,37 @@ function updateList() {
       updateList();
     });
 
-    // Remove Button
+    // Edit Button
     buttons[1].addEventListener("click", function () {
+      toggleListMaker(true, i);
+    });
+
+    // Remove Button
+    buttons[2].addEventListener("click", function () {
       list.splice(i, 1);
       updateList();
     });
 
     listContents.appendChild(div);
+  }
+
+  updateListInfo();
+}
+
+function updateListInfo() {
+  let viewForm = document.getElementById("view-type-form");
+  let viewType = viewForm["view-type"].value;
+  let listInfo = document.getElementById("list-info");
+
+  switch (viewType) {
+    case "view-to-do":
+      listInfo.innerText = `${list.filter((elm) => !elm.finished).length}개의 리스트`;
+      break;
+    case "view-complete":
+      listInfo.innerText = `${list.filter((elm) => elm.finished).length}개의 리스트`;
+      break;
+    default:
+      listInfo.innerText = `${list.length}개의 리스트`;
+      break;
   }
 }
