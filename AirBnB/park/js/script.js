c944c99ed949183fc1ce27da81b19efb2ed2637e
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2024-10-29 09:05:36 +0900

    [박창조][5주차] 무한스크롤 일부 구현

--- AirBnB/park/js/script.js
+++ AirBnB/park/js/script.js
@@ -27,4 +27,36 @@ window.onload = async () => {
   document
     .querySelector('#category_right_button')
     .addEventListener('click', handleRightButton);
+
+  // infinite scroll
+
+  const cardList = document.querySelector('#cardList ul');
+
+  const infiniteScroll = (entries, observer) => {
+    entries.forEach(async (entry) => {
+      // isIntersecting : 교차상태일 때
+      if (entry.isIntersecting) {
+        // Room Data 가져오기
+        const data = await fetchData('/AirBnB/park/assets/data/data.json');
+        setRoomList(data);
+        setList();
+
+        // observer 지우고 새로 등록
+        observer.unobserve(entry.target);
+
+        const newElement = cardList.lastElementChild;
+        console.log(newElement);
+
+        newElement && observer.observe(newElement);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(infiniteScroll, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.75,
+  });
+
+  observer.observe(cardList.lastElementChild);
 };
